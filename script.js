6067a09239ac283fe37b025761e9b6361ff45493
Ricardo Haduca <ibrigitteaeyahoo@gmail.com>
2023-10-05 13:31:20 +0800

    feat: added javascript into my html

---
+++ script.js
@@ -0,0 +1,78 @@
+let firstName = "Brigitte Mae";
+let last_name = " Ibeng";
+let fullName = firstName + ' ' + last_name;
+console.log (fullName);
+console.log("BSCS-2A");
+console.log("CS201-BMI-LAB2");
+
+let firstFavNum = 9;
+let secondFavNum = 8;
+
+let g = firstFavNum > secondFavNum;
+let l = firstFavNum < secondFavNum;
+let x = firstFavNum <= secondFavNum;
+let y = firstFavNum === secondFavNum;
+let r = firstFavNum == secondFavNum;
+let b = firstFavNum !== secondFavNum;
+let f = firstFavNum != secondFavNum;
+console.log("The first variable is greater then the second variable: " + g);
+console.log("The first variable is less then the second variable: " + l);
+console.log ("First number is less than and equal to second number: " + x);
+console.log("FirstFavNum is equal to secondFavNum: " + y);
+console.log("FirstFavNum is equal to secondFavNum: " + r);
+console.log("FirstFavNum is not equal to secondFavNum: " + b);
+console.log("FirstFavNum is not equal to secondFavNum: " + f);
+//checks if the first var is greater than the second var
+// if (firstFavNum > secondFavNum) {
+//     console.log ( "First number is greater than the second number");
+//     }
+//         else {
+//      console.log ("Second number is greater than the first number");  
+//     }
+
+//checks if the first var is less then the second var
+// if (firstFavNum < secondFavNum) {
+//     console.log ( "first number is less then the second number");
+//     }
+//         else {
+//      console.log ("first number is greater than the  second number");  
+//     }
+ 
+// checks if first var is greater than or equal to second var
+// if (firstFavNum <= secondFavNum) {
+//     console.log ( "First number is less than and equal to second number");
+//     }
+//         else {
+//      console.log ("Second number is greater than and not equal to first number");  
+//     }
+// checks if first var is equal to second var using strict equality operator
+// if (firstFavNum === secondFavNum) {
+//     console.log(" firstFavNum is equal to secondFavNum");
+// }
+//     else {
+//     console.log("firstFavNum is not equal to secondFavnum");
+//     }
+
+// checks if first var is equal to second var using loose equality operator
+// if (firstFavNum == secondFavNum) {
+//     console.log(" firstFavNum is equal to secondFavNum");
+// }
+//     else {
+//     console.log("firstFavNum is not equal to secondFavnum");
+//     }
+
+// checks if first var is not equal to second var using non - strict equality operator
+// if (firstFavNum !== secondFavNum) {
+//     console.log("firstFavNum is not equal to secondFavNum");
+// }
+//     else {
+//     console.log("firstFavNum is  equal to secondFavnum");
+//     }
+
+// checks if first var is equal to second var using non - loose equality operator
+// if (firstFavNum != secondFavNum) {
+//     console.log("firstFavNum is not equal to secondFavNum");
+// }
+//     else {
+//     console.log("firstFavNum is  equal to secondFavnum");
+//     }
